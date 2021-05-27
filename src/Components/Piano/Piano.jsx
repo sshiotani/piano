af92715be9e8cc2a79c547f2795e3e365fd46f14
Shiotani <M10541@NGC.AUTHORITY>
2021-05-27 14:18:42 -0600

    display history

--- src/Components/Piano/Piano.jsx
+++ src/Components/Piano/Piano.jsx
@@ -1,25 +1,30 @@
-import React, { Fragment,useState } from "react"
+import React, { Fragment, useState } from "react"
 import { Key } from "../Key/Key"
 import "./Piano.css"
 
+export const Piano = ({ keys }) => {
+	const [keyPressed, setKeyPressed] = useState([])
 
-
-export const Piano = ({keys}) => {
-    const [keyPressed, setKeyPressed] = useState([])
-
-    const handleClick = (key)=> {
-        setKeyPressed([...keyPressed, key])
-    }
+	const handleClick = (key) => {
+		setKeyPressed([...keyPressed, key])
+	}
 
 	return (
-		<div className="piano">
-			{keys.map((key) => {
-				return (
-					<Fragment>
-						<Key key={key} note={key} onClick={()=>handleClick(key)}></Key>
-					</Fragment>
-				)
-			})}
+		<div>
+			<div className="piano">
+				{keys.map((key) => {
+					return (
+						<Fragment>
+							<Key
+								key={key}
+								note={key}
+								onClick={() => handleClick(key)}
+							></Key>
+						</Fragment>
+					)
+				})}
+			</div>
+			<div>{keyPressed}</div>
 		</div>
 	)
 }
